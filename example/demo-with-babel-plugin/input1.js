49e5aabb18c8642273f94e670f4eb93ddd901562
student0176 <shisu0176@email.com>
2023-11-08 20:36:50 +0800

    mit_start

---
+++ example/demo-with-babel-plugin/input1.js
@@ -0,0 +1,18 @@
+import increaser from './input2'
+var num = 1
+var str = 'foo'
+var boo = false
+var fn = () => {
+    console.log(boo)
+}
+var b = window.localStorage.getItem(str)
+var num2 = increaser(num)
+
+fn()dd
+console.log(num2, boo)
+alert(b)
+
+
+sdfefsf
+dfefe
+effffa
